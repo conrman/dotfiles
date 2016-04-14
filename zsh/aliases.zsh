a799877e0a5ca044ffb0c078b3073d3fcbb00d39
conrman <connormfreeman@gmail.com>
2016-04-13 22:54:48 -0500

    Cleaning up and adding neovim taskrunner plugin

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,4 +5,4 @@ alias vh='vagrant halt'
 alias vs='vagrant status'
 alias vp='vagrant provision'
 
-
+alias homestead='function __homestead() { (cd ~/.homestead && vagrant $*); unset -f __homestead; }; __homestead'
