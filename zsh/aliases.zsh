b0f40c3016f4621ce84dc2c5f03d93a54ccc49fc
conrman <connormfreeman@gmail.com>
2016-05-01 19:20:54 -0500

    Adding some new aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,11 @@ alias vu='vagrant up'
 alias vh='vagrant halt'
 alias vr='vagrant reload'
 alias vs='vagrant status'
+alias vgs='vagrant global-status'
 alias vp='vagrant provision'
 
 alias homestead='function __homestead() { (cd ~/.homestead && vagrant $*); unset -f __homestead; }; __homestead'
+
+alias art='php artisan'
+
+alias dev='cd ~/dev'
