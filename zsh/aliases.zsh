d6be3f95bfdcd60220f6b36c00163910d6223fb5
Connor Freeman <connor_freeman@apple.com>
2017-09-16 12:28:27 -0500

    Added sublime settings

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,6 +16,7 @@ alias vgs='vagrant global-status'
 alias vp='vagrant provision'
 
 alias homestead='function __homestead() { (cd ~/.homestead && vagrant $*); unset -f __homestead; }; __homestead'
+alias phpmamp='/Applications/MAMP/bin/php/php5.6.28/bin/php';
 
 alias art='php artisan'
 
